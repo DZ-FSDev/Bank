4fc83b88e4e3afbb385c994612ab59cb4d5cf0eb
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-24 13:05:54 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -4,9 +4,8 @@
  * PURPOSE:  Defines a bank of clients and accounts.
  */
 public class Bank{
-  
-  private final int MAX_CLIENTS = 1000;  //Maximum number of clients [People]
-  private final int MAX_ACCOUNTS = 1000;  //Maximum number of clients
+  private static final int MAX_CLIENTS = 1000;  //Maximum number of clients [People]
+  private static final int MAX_ACCOUNTS = 1000;  //Maximum number of accounts
   private BankClient[] clients;  //Array of clients
   private BankAccount[] accounts;  //Array of accounts
   private int numClients;  //Number of clients in bank
@@ -16,26 +15,28 @@ public class Bank{
   * Contructor of bank.  Initializes arrays for the clients and accounts.
   */  
   public Bank(){
-    clients = new BankClient[MAX_CLIENTS];
-    accounts = new BankAccount[MAX_ACCOUNTS];
-    numClients = 0;
-    numAccounts = 0;
+    this.clients = new BankClient[MAX_CLIENTS];
+    this.accounts = new BankAccount[MAX_ACCOUNTS];
+    this.numClients = 0;
+    this.numAccounts = 0;
   }
 
  /*
   * Adds a client to the bank of first and last name.
   */    
   public void addBankClient(String firstName, String lastName){
-    clients[numClients ++] = new BankClient(firstName, lastName, (1019999999999999l + numClients));                                                 
+    this.clients[this.numClients ++] =
+      new BankClient(firstName, lastName, (1019999999999999l + this.numClients));                                                 
   }
   
  /*
   * Returns the client of the bank which matches with the first and last name provided.
-  */    
+  */
   public BankClient getBankClient(String firstName, String lastName){
-    for(int i = 0; i < numClients; i++){
-      if(clients[i].getFirstName().equals(firstName) && clients[i].getLastName().equals(lastName)){
-        return clients[i];
+    for(int i = 0; i < this.numClients; i++){
+      if(this.clients[i].getFirstName().equals(firstName) &&
+         this.clients[i].getLastName().equals(lastName)) {
+        return clients[i].clone();
       }
     }
     return null;
@@ -45,16 +46,16 @@ public class Bank{
   * Adds a new chequing account to the bank for the client with the beginning balance provided.
   */    
   public int addChequingAccount(BankClient client, double balance){
-    accounts[numAccounts ++] = new ChequingAccount(client, balance, (499999 + numAccounts));
-    return (499999 + numAccounts);
+    this.accounts[this.numAccounts ++] = new ChequingAccount(client, balance, (499999 + this.numAccounts));
+    return (499999 + this.numAccounts);
   }
   
  /*
   * Adds a new savings account to the bank for the client with the beginning balance provided.
   */    
   public int addSavingsAccount(BankClient client, double balance){
-    accounts[numAccounts ++] = new SavingsAccount(client, balance, (499999 + numAccounts));
-    return (499999 + numAccounts);
+    accounts[numAccounts ++] = new SavingsAccount(client, balance, (499999 + this.numAccounts));
+    return (499999 + this.numAccounts);
   }
   
  /*
@@ -63,7 +64,7 @@ public class Bank{
   public BankAccount getBankAccount(int id){
     for(int i = 0; i < numAccounts; i++){
       if(accounts[i].getID() == id){
-        return accounts[i];
+        return this.accounts[i];
       }
     }
     return null;
@@ -74,7 +75,7 @@ public class Bank{
   */    
   public void payInterest(){
     for(int i = 0; i < numAccounts; i++){
-      accounts[i].collectInterest();
+      this.accounts[i].collectInterest();
     }
   }
 }
