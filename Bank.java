bee8cab2706cacdc09e5380429f8c8cb840f69ee
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-24 13:16:47 -0600

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,18 +1,30 @@
-/*
- * Bank
+/**
+ * Defines a bank of clients and accounts.
  * 
- * PURPOSE:  Defines a bank of clients and accounts.
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.2
  */
 public class Bank{
-  private static final int MAX_CLIENTS = 1000;  //Maximum number of clients [People]
+  /** The maximum number of clients this bank will handle. */
+  private static final int MAX_CLIENTS = 1000;
+  
+  /** The maximum number of accounts this bank will handle. */
   private static final int MAX_ACCOUNTS = 1000;  //Maximum number of accounts
+  
+  /** The starting client ID. */
+  private static final long CLIENT_ID_BASE = 1019999999999999l;
+  
+  /** The starting account ID. */
+  private static final long ACCOUNT_ID_BASE = 499999;
+  
   private BankClient[] clients;  //Array of clients
   private BankAccount[] accounts;  //Array of accounts
-  private int numClients;  //Number of clients in bank
-  private int numAccounts;  //Number of accounts in bank
+  private long numClients;  //Number of clients in bank
+  private long numAccounts;  //Number of accounts in bank
   
- /*
-  * Contructor of bank.  Initializes arrays for the clients and accounts.
+ /**
+  * Contructs a new bank.
   */  
   public Bank(){
     this.clients = new BankClient[MAX_CLIENTS];
@@ -21,12 +33,15 @@ public class Bank{
     this.numAccounts = 0;
   }
 
- /*
-  * Adds a client to the bank of first and last name.
+ /**
+  * Adds a client to the bank with a specified first and last name.
+  *
+  * @param firstName The first name of the client.
+  * @param lastName The last name of the client.
   */    
   public void addBankClient(String firstName, String lastName){
     this.clients[this.numClients ++] =
-      new BankClient(firstName, lastName, (1019999999999999l + this.numClients));                                                 
+      new BankClient(firstName, lastName, (CLIENT_ID_BASE + this.numClients));                                                 
   }
   
  /*
@@ -46,16 +61,16 @@ public class Bank{
   * Adds a new chequing account to the bank for the client with the beginning balance provided.
   */    
   public int addChequingAccount(BankClient client, double balance){
-    this.accounts[this.numAccounts ++] = new ChequingAccount(client, balance, (499999 + this.numAccounts));
-    return (499999 + this.numAccounts);
+    this.accounts[this.numAccounts ++] = new ChequingAccount(client, balance, (ACCOUNT_ID_BASE + this.numAccounts));
+    return (ACCOUNT_ID_BASE + this.numAccounts);
   }
   
  /*
   * Adds a new savings account to the bank for the client with the beginning balance provided.
   */    
   public int addSavingsAccount(BankClient client, double balance){
-    accounts[numAccounts ++] = new SavingsAccount(client, balance, (499999 + this.numAccounts));
-    return (499999 + this.numAccounts);
+    accounts[numAccounts ++] = new SavingsAccount(client, balance, (ACCOUNT_ID_BASE + this.numAccounts));
+    return (ACCOUNT_ID_BASE + this.numAccounts);
   }
   
  /*
