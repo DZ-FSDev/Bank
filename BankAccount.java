6876833230b9aadd9743330545cfbf8b31551151
DZ-FSDev <85122787+DZ-FSDev@users.noreply.github.com>
2021-12-26 19:07:31 -0600

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -5,7 +5,7 @@
  * @since 17.0.1
  * @version 0.0.2
  */
-public abstract class BankAccount{
+public abstract class BankAccount implements Clonable{
   private final BankClient OWNER;  //Owner of the bank account.
   private double balance;  //Balance of the bank account.
   private final int ID;  //ID of the bank account.
@@ -16,7 +16,7 @@ public abstract class BankAccount{
   * @param owner The specified owner of this bank account.
   * @param balance The specified starting balance of this bank account.
   * @param id The specified id of this bank account.
-  * @throws IllegalArgumentException Thrown when an invalid owner or ID was specified.
+  * @throws IllegalArgumentException Thrown when an invalid owner, balance, or ID was specified.
   */
   public BankAccount(BankClient owner, double balance, int id) throws IllegalArgumentException {
     this.OWNER = owner;
@@ -28,22 +28,43 @@ public abstract class BankAccount{
   * Sets the balance of this bank account to a new balance.
   *
   * @param newBalance The new balance for this bank account.
+  * @throws IllegalArgumentException Thrown when a non-number is provided.
   */
-  public void setBalance(double newBalance) {
+  public void setBalance(double newBalance) throws IllegalArgumentException{
+    if(Double.isNaN(newBalance) || Double.isInfinite(newBalance)) {
+      throw new IllegalArgumentException(newBalance + " is not a valid balance.");
+    }
     
+    this.balance = newBalance;
   }
   
  /**
-  * Adds the provided amount to the balance of the bank account.
+  * Adds the provided amount to the balance of the bank account as a deposit.
+  *
+  * @param amount The specified amount to deposit.
+  * @throws IllegalArgumentException Thrown when a non-number is provided.
   */    
-  public void deposit(double amount){
+  public void deposit(double amount) throws IllegalArgumentException{
+    if(Double.isNaN(amount) || Double.isInfinite(amount)) {
+      throw new IllegalArgumentException(amount + " is not a valid balance.");
+    }
+    
     balance += amount;
   }
   
- /*
-  * Subtracts the provided amount from the balance of the bank account.
+ /**
+  * Subtracts the provided amount from the balance of the bank account as a withdrawal.
+  *
+  * @param amount The specified amount to be withdrawn.
+  * @throws InsufficientFundsException Thrown when there is not enough funds for the
+  *         withdrawal.
+  * @throws IllegalArgumentException Thrown when a non-number is provided.
   */    
-  public void withdraw(double amount)throws InsufficientFundsException{
+  public void withdraw(double amount)throws InsufficientFundsException, IllegalArgumentException{
+    if(Double.isNaN(amount) || Double.isInfinite(amount)) {
+      throw new IllegalArgumentException(amount + " is not a valid balance.");
+    }
+    
     if((balance - amount) > 0){
       balance -= amount;
     }else{
@@ -51,8 +72,10 @@ public abstract class BankAccount{
     }
   }
   
- /*
+ /**
   * Returns the ID of the bank account.
+  * 
+  * @return The ID of the bank account.
   */    
   public int getID(){
     return id;
@@ -62,6 +85,15 @@ public abstract class BankAccount{
   * Collects interest for the bank account.
   */    
   public abstract void collectInterest();
+  
+ /**
+  * Returns a deep clone of this Bank Account.
+  * 
+  * @return A deep clone of this Bank Account.
+  */
+  protected BankAccount clone(){
+    return new BankAccount(owner.clone(), balance, id);
+  }
   
  /**
   * Returns the description of the bank account, including the owner, ID, and the balance.
