aac9d3e102ff562b1f388deb95d8654f33cdad3f
JZ-FSDev <94236556+JZ-FSDev@users.noreply.github.com>
2022-01-08 23:14:58 -0600

    Update InsufficientFundsException.java

--- InsufficientFundsException.java
+++ InsufficientFundsException.java
@@ -1,12 +1,16 @@
-/*
- * InsufficientFundsException
- * 
- * PURPOSE:  Defines the exception to be thrown when there are insufficient funds in the account during a withdrawal attempt.
+/**
+ * Defines the exception to be thrown when there are insufficient funds in the account during a withdrawal attempt.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
  */
 public class InsufficientFundsException extends Exception {
-  
- /*
-  * Constructor of insufficient funds exception.  Calls the super constructor and passes a message provided as a parameter.
+ /**
+  * Contructs a new insufficient funds exception.  
+  * Calls the super constructor and passes a message provided as a parameter.
+  *
+  * @param message The specified exception message to be passed to the super constructor.
   */    
   public InsufficientFundsException(String message){
     super(message);
