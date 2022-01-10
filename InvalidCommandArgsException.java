5ee06acae0e066eb1e11f844d4decac9d4cb690c
JZ-FSDev <94236556+JZ-FSDev@users.noreply.github.com>
2022-01-09 22:19:33 -0600

    Update InvalidCommandArgsException.java

--- InvalidCommandArgsException.java
+++ InvalidCommandArgsException.java
@@ -1,12 +1,16 @@
-/*
- * InvalidCommandArgsException
- * 
- * PURPOSE: Defines the exception to be thrown when an invalid argument is read by the parser.
+/**
+ * Defines the exception to be thrown when an invalid argument is read by the parser.
+ *
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
  */
 public class InvalidCommandArgsException extends WrongCommandException{
-  
- /*
-  * Constructor of insufficient funds exception.  Calls the super constructor and passes a message provided as a parameter.
+ /**
+  * Constructor of insufficient funds exception.  
+  * Calls the super constructor and passes a message provided as a parameter.
+  *
+  * @param message The specified exception message to be passed to the super constructor.
   */    
   public InvalidCommandArgsException(String message){
     super(message);
