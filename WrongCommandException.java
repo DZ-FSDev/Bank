d22c3f5c1c2ebdf61bb27790e0c727926f420412
JZ-FSDev <94236556+JZ-FSDev@users.noreply.github.com>
2022-01-12 23:22:01 -0600

    Update WrongCommandException.java

--- WrongCommandException.java
+++ WrongCommandException.java
@@ -1,12 +1,16 @@
-/*
- * WrongCommandException
+/**
+ * Defines the exception to be thrown when an wrong command is read by the parser.
  * 
- * PURPOSE: Defines the exception to be thrown when an wrong command is read by the parser.
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
  */
 public class WrongCommandException extends Exception{
-  
- /*
-  * Constructor of wrong command exception.  Calls the super constructor and passes a message provided as a parameter.
+ /**
+  * Constructor of wrong command exception.  
+  * Calls the super constructor and passes a message provided as a parameter.
+  *
+  * @param message The specified exception message to be passed to the super constructor.
   */    
   public WrongCommandException(String message){
     super(message);
