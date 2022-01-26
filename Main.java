1744fa2b0878654fa40b6e35e34385fcb2f5393f
JZ-FSDev <jzcompsci@gmail.com>
2022-01-26 15:40:45 -0600

    Updated documentation

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
-/*
- * Main
- * 
- * PURPOSE: Reads input and parses through the commands of a provided file.  Exceptions are raised for the commands
- *          which encounter errors.
- */
-
 import java.io.*;
 import java.util.*;
+import java.math.BigDecimal;
+
+/**
+ * Reads input and parses through the commands of a provided file.  
+ * Exceptions are raised for the commands which encounter errors.
+ * 
+ * @author JZ-FSDev
+ * @since 17.0.1
+ * @version 0.0.1
+ */
 public class Main{
   
-  private final static String FILE_NAME = "inputPhase4.txt";  //Completed phase 4
+  private final static String FILE_NAME = "TEST_FILE.txt";
   
- /*
-  * Initiates the reading of the file.
+ /**
+  * Initiates the reading of the file and performs commands according to the input file provided.
   */    
   public static void main(String[] args){
     Bank bank = new Bank();
@@ -23,8 +26,11 @@ public class Main{
     }
   }
   
- /*
-  * Reads the provided file line by line and passes it to the parse command method.
+ /**
+  * Reads the specified file line by line and passes it to the parse command method.
+  *
+  * @param file The input file containing the commands to be excuted.
+  * @param bank The bank for the commands to be executed on.
   */    
   public static void readInputFile(String file, Bank bank) throws Exception{
     BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -39,8 +45,12 @@ public class Main{
     reader.close();
   }
   
- /*
-  * Performs the required actions by reading the provided tokens one by one.
+ /**
+  * Performs the required actions based on the specified command line by reading the provided tokens one by one.
+  *
+  * @param line The command line to be parsed.
+  * @param bank The bank for the commands to be excuted on.
+  * @throws WrongCommandException Thrown when an unrecognized command is parsed.
   */    
   public static void parseCommand(String line, Bank bank) throws Exception{
     Scanner lineReader = new Scanner(line);
@@ -71,15 +81,21 @@ public class Main{
     }
   }
   
- /*
-  * Prints out a comment.
+ /**
+  * Prints out a specified line.
+  *
+  * @param line The line to be printed.
   */    
   public static void processComment(String line){
     System.out.println(line);
   }
   
- /*
+ /**
   * Adds a new client to the bank.  Throws an exception if any errors are encountered.
+  *
+  * @param scanner The Scanner to read the next two tokens of first name then last name.
+  * @param bank The bank for the new client to be added to.
+  * @throws InvalidCommandArgsException Thrown when either a first or last name is missing.
   */    
   public static void newClient(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     try{
@@ -90,8 +106,12 @@ public class Main{
     }
   }
   
- /*
+ /**
   * Prints the account info of a bank client.  Throws an exception if any errors are encountered.
+  *
+  * @param scanner The Scanner to read the next two tokens of first name then last name.
+  * @param bank The bank for the client info to be retrieved from.
+  * @throws InvalidCommandArgsException Thrown when either a first or last name is missing.
   */    
   public static void getClientInfo(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     try{
@@ -106,18 +126,22 @@ public class Main{
     }
   }
   
- /*
+ /**
   * Creates a new account for a bank client.  Throws an exception if any errors are encountered.
+  *
+  * @param scanner The Scanner to read the next four tokens of bank accoun type, first name, last name, then opening balance.
+  * @param bank The bank for the new account to be added to.
+  * @throws InvalidCommandArgsException Thrown when either an account type, first or last name, or opening balance is missing or invalid.
   */    
   public static void newAccount(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     BankClient client;
     String type, first, last;
-    double balance;
+    BigDecimal balance;
     try{
       type = scanner.next();
       first = scanner.next();
       last = scanner.next();
-      balance = scanner.nextDouble();
+      balance = BigDecimal.valueOf(scanner.nextDouble());
       client = bank.getBankClient(first, last);
     }catch(Exception e){
       throw new InvalidCommandArgsException("NEW-ACCOUNT: account type, first name, last name or balance is missing.");
@@ -135,8 +159,12 @@ public class Main{
     }
   }
   
- /*
-  * Prints the account info a bank account.  Throws an exception if any errors are encountered.
+ /**
+  * Prints the account info a bank account.  Throws an exception the account of specified ID is not found or missing.
+  *
+  * @param scanner The Scanner to read the next token of bank ID.
+  * @param bank The bank for the account info to be retrieved from.
+  * @throws InvalidCommandArgsException Thrown when either an invalid bank ID is provided.
   */    
   public static void getAccountInfo(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     try{
@@ -151,13 +179,17 @@ public class Main{
     }
   }
   
- /*
-  * Processes a deposit on a bank account.  Throws an exception if any errors are encountered.
+ /**
+  * Processes a deposit on a bank account.  Throws an exception the account of specified ID is not found or missing.
+  *
+  * @param scanner The Scanner to read the next token of bank ID.
+  * @param bank The bank to process the deposit.
+  * @throws InvalidCommandArgsException Thrown when either an invalid bank ID is provided.
   */    
   public static void deposit(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     try{
       BankAccount account = bank.getBankAccount(scanner.nextInt());
-      double amount = scanner.nextDouble();
+      BigDecimal amount = BigDecimal.valueOf(scanner.nextDouble());
       if(account == null){
         System.out.println("ACCOUNT NOT FOUND");
       }else{
@@ -169,13 +201,17 @@ public class Main{
     }
   }
   
- /*
-  * Processes a withdrawal on a bank account.  Throws an exception if any errors are encountered.
+ /**
+  * Processes a withdrawal on a bank account.  Throws an exception the account of specified ID is not found or missing.
+  *
+  * @param scanner The Scanner to read the next token of bank ID.
+  * @param bank The bank to process the withdrawal.
+  * @throws InvalidCommandArgsException Thrown when either an invalid bank ID is provided.
   */    
   public static void withdraw(Scanner scanner, Bank bank) throws InvalidCommandArgsException{
     try{
       BankAccount account = bank.getBankAccount(scanner.nextInt());
-      double amount = scanner.nextDouble();
+      BigDecimal amount = BigDecimal.valueOf(scanner.nextDouble());
       if(account == null){
         System.out.println("ACCOUNT NOT FOUND");
       }else{
@@ -191,8 +227,10 @@ public class Main{
     }
   }
   
- /*
-  * Pays the interest to the savings accounts of the bank.
+ /**
+  * Pays the interest to the savings accounts of the specified bank.
+  *
+  * @param bank The bank to process the interest payment.
   */    
   public static void interest(Bank bank){
     bank.payInterest();
